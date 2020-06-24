f66486a7b42faeaa9194f2538e866b422eccc0bb
sisilhowie99 <sisil.anastasia@gmail.com>
2020-06-24 19:08:17 +1000

    added functionality for removing songs from playlist

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -53,6 +53,7 @@ class App extends React.Component {
     };
 
     this.addTrack = this.addTrack.bind(this);
+    this.removeTrack = this.removeTrack.bind(this);
   }
 
   addTrack(track) {
@@ -62,6 +63,11 @@ class App extends React.Component {
       this.state.playlistTracks.push(track);
       this.setState({ playlistTracks: this.state.playlistTracks});
     }
+  }
+
+  removeTrack(track) {
+    let reducedPlaylist = this.state.playlistTracks.filter(savedTrack => savedTrack.id !== track.id);
+    this.setState({playlistTracks: reducedPlaylist});
   }
 
   render() {
@@ -76,7 +82,8 @@ class App extends React.Component {
               onAdd={this.addTrack} />
             <Playlist 
               playlistName={this.state.playlistName} 
-              playlistTracks={this.state.playlistTracks} />
+              playlistTracks={this.state.playlistTracks} 
+              onRemove={this.removeTrack} />
           </div>
         </div>
       </div>
