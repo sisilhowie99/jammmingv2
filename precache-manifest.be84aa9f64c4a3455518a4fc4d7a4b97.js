c47863ce0f51cfba682eb917ed139c57f0929b21
sisilhowie99 <sisil.anastasia@gmail.com>
2020-12-30 22:14:08 +1000

    Updates

---
+++ precache-manifest.be84aa9f64c4a3455518a4fc4d7a4b97.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef04e1e3bdb83ab12365a9433b0ee0ac",
+    "url": "/jammmingv2/index.html"
+  },
+  {
+    "revision": "65c466f54e12ba055209",
+    "url": "/jammmingv2/static/css/main.de340902.chunk.css"
+  },
+  {
+    "revision": "1aa79388e053e435a95a",
+    "url": "/jammmingv2/static/js/2.57aae4fa.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/jammmingv2/static/js/2.57aae4fa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "65c466f54e12ba055209",
+    "url": "/jammmingv2/static/js/main.572072e9.chunk.js"
+  },
+  {
+    "revision": "874b1bbc6687a84301b7",
+    "url": "/jammmingv2/static/js/runtime-main.7e13a01a.js"
+  },
+  {
+    "revision": "37be7357a5f8583073df7e0aec787eaa",
+    "url": "/jammmingv2/static/media/background_photo_desktop.37be7357.jpg"
+  }
+]);
